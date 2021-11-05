5d766e7d26b022c21aeb9ea0aae8401ed29bb32a
NB993 <qnalsfj@gmail.com>
2021-11-05 15:06:57 +0900

    백준 11654번

---
+++ src/beakjoon/basic/Main11654.java
@@ -0,0 +1,12 @@
+package beakjoon.basic;
+
+import java.util.*;
+
+public class Main11654 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println(((int) sc.next().charAt(0)));
+  }
+}
