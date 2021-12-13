18c19369b4515df2251e01ccfa3de57a25ddcfd1
Jaehong Choe <qnalsfj@gmail.com>
2021-12-13 23:05:27 +0900

    Change code after seeing the answer code
    답안 예시 보고 수정하였음.

--- src/nadongbin/part2/greedy/Greedy_3.java
+++ src/nadongbin/part2/greedy/Greedy_3.java
@@ -10,23 +10,28 @@ public class Greedy_3 {
 
     int n = sc.nextInt(); //행
     int m = sc.nextInt(); //열
+    int result = 0;
 
-    int[][] arr = new int[n][m];
+    //굳이 for문 한 번 더 돌면서 숫자 담을 필요 없음.
+//    int[][] arr = new int[n][m];
+//    for (int i = 0; i < n; i++) {
+//      for (int j = 0; j < m; j++) {
+//        arr[i][j] = sc.nextInt();
+//      }
+//    }
+
+    // 행과 열 길이를 아니까 nextInt()받으면서 비교해도 됨.
     for (int i = 0; i < n; i++) {
+      int minNumber = 10001; //10000 이하의 수가 입력됨
       for (int j = 0; j < m; j++) {
-        arr[i][j] = sc.nextInt();
+        int x = sc.nextInt();
+        minNumber = Math.min(minNumber, x); //min() : 둘 중 작은 수를 리턴함
       }
+      // '가장 작은 수'들 중에서 가장 큰 수 찾기
+      result = Math.max(result, minNumber); //max() : 둘 중 큰 수를 리턴함
     }
 
-    int biggest = 0;
-    for (int i = 0; i < i; i++) {
-      Arrays.sort(arr[i]);
-      for (int j = 0; j < m; j++) {
-        if (arr[i][0] > biggest) {
-          biggest = arr[i][0];
-        }
-      }
-    }
+    System.out.println(result);
 
   }
 
