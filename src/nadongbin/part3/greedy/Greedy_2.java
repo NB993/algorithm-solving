8aabeddcdc5056e49133ef77b6cd14c5f9326fc8
Jaehong Choe <qnalsfj@gmail.com>
2021-12-15 01:48:10 +0900

    Add answer code
    charAt() - '0' 꿀팁 하나 배웠다.

--- src/nadongbin/part3/greedy/Greedy_2.java
+++ src/nadongbin/part3/greedy/Greedy_2.java
@@ -6,22 +6,24 @@ public class Greedy_2 {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    String[] numArr = sc.nextLine().split("");
+    String str = sc.nextLine();
 
-//    String[] numArr = {"5", "6", "7"};
+    // split할 필요 없이 그냥 char배열처럼 사용해버리기.
+    // '0'의 아스키코드 값이 48이고 '9'까지 아스키코드 값이 1씩 증가하므로 결국 아래 코드는
+    // 문자열 숫자와 동일한 int/long 값을 리턴할 수 있음.
+    long result = str.charAt(0) - '0'; //첫문자를 일단 숫자로 바꿔서 저장해놓고,
 
-    int result = 0;
-    int currentNumber = 0;
-    for (int i = 0; i < numArr.length; i++) {
-      currentNumber = Integer.parseInt(numArr[i]);
-      if (currentNumber == 0 || currentNumber == 1 || result == 0) {
-        result += currentNumber;
+    //두 번째 숫자부터 반복문 수행.
+    for (int i = 1; i < str.length(); i++) {
+      int num = str.charAt(i) - '0';
+      if (num <= 1 || result <= 1) {
+        result += num;
       } else {
-        result *= currentNumber;
+        result *= num;
       }
     }
-    System.out.println(result);
 
+    System.out.println(result);
   }
 
 }
