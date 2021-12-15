5d0a617e3766b521bfa4c0c144b7f9a2caf912b5
Jaehong Choe <qnalsfj@gmail.com>
2021-12-16 00:35:21 +0900

    Add someone's code
    BufferedReader때문인지 시간도 나보다 절반밖에 안걸리고, 분기문도 깔끔해서
    참고용으로 추가.

--- src/nadongbin/part3/greedy/Greedy_3.java
+++ src/nadongbin/part3/greedy/Greedy_3.java
@@ -1,32 +1,50 @@
 package nadongbin.part3.greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Greedy_3 {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-//    String s = sc.nextLine();
+  public static void main(String[] args) throws IOException {
+    //백준 01089292615님 코드
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    char[] input = br.readLine().toCharArray();
+    char temp = input[0];
 
-    String s = "0101010101";
-    int seperateCount = 0;
-    for (int i = 0; i < s.length() - 1; i++) {
-      if (s.charAt(i) != s.charAt(i + 1)) {
-        seperateCount++;
+    //1을 카운트 해놓고 시작하니까 나처럼 분기문을 작성할 필요가 없음. 앞으로 이 방식 쓰면 좋을듯.
+    int answer = 1;
+    for (int i = 1; i < input.length; i++) {
+      if (temp != input[i]) {
+        temp = input[i];
+        answer++;
       }
     }
-    int result = 0;
-    if (seperateCount == 0) {
-      System.out.println(result);
-      return;
-    }
-    if (seperateCount % 2 != 0) {
-      result = seperateCount / 2 + 1;
-    } else {
-      result = seperateCount / 2;
-    }
-    System.out.println(result);
+    System.out.println(answer / 2);
 
+    //내 코드
+//    Scanner sc = new Scanner(System.in);
+////    String s = sc.nextLine();
+//
+//    String s = "0101010101";
+//    int seperateCount = 0;
+//    for (int i = 0; i < s.length() - 1; i++) {
+//      if (s.charAt(i) != s.charAt(i + 1)) {
+//        seperateCount++;
+//      }
+//    }
+//    int result = 0;
+//    if (seperateCount == 0) {
+//      System.out.println(result);
+//      return;
+//    }
+//    if (seperateCount % 2 != 0) {
+//      result = seperateCount / 2 + 1;
+//    } else {
+//      result = seperateCount / 2;
+//    }
+//    System.out.println(result);
 
   }
 
