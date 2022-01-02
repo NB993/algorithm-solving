3632debadbeb65f3793ae188058c3f84aa406081
Jaehong Choe <qnalsfj@gmail.com>
2022-01-02 16:16:31 +0900

    [greedy] 백준 2217번 - 로프
    - Scanner -> BufferedReader로 변경.
    소모된 메모리와 시간이 절반 이하로 감소.

--- src/beakjoon/greedy/Main_2217.java
+++ src/beakjoon/greedy/Main_2217.java
@@ -1,21 +1,26 @@
 package beakjoon.greedy;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main_2217 {
   //로프
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
+  public static void main(String[] args) throws IOException {
+    //Scanner sc = new Scanner(System.in);
+    //int N = sc.nextInt();
+    //int[] arr = new int[N];
+    //sc.nextLine();
+
+    //BufferdReader가 Scanner보다 메모리랑 시간을 1/4 ~ 1/3밖에 소모하지 않았음
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
     int[] arr = new int[N];
-    sc.nextLine();
-
-    for (int i = 0; i < N; i++) {
-      arr[i] = sc.nextInt();
+    for(int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
     }
-    Arrays.sort(arr);
 
+    Arrays.sort(arr);
     int max = arr[0] * N;
     for (int i = 1; i < N - 1; i++) {
       if (arr[i] * (N - i) > max) {
