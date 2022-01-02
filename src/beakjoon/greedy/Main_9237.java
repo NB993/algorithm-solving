0d2c6e5a9fb11d1bf84f2bbe2b70e28c2891ebc1
Jaehong Choe <qnalsfj@gmail.com>
2022-01-02 17:09:48 +0900

    [greedy] 백준 9237번 - 이장님 초대
    - stringtokenizer사용법과 문제풀 때 배열 반대로 사용하는 거 안 헷갈리게
    숙달시켜야 할듯.

--- src/beakjoon/greedy/Main_9237.java
+++ src/beakjoon/greedy/Main_9237.java
@@ -1,2 +1,32 @@
-package beakjoon.greedy;public class Main_9237 {
+package beakjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_9237 {
+  //이장님 초대
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int[] arr = new int[N];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+    Arrays.sort(arr);
+
+    int day = 1;
+    int max = arr[N - 1] + day;
+    for (int i = N - 1; i > 0; i--) {
+      day++;
+      if (max < arr[i - 1] + day) {
+        max = arr[i - 1] + day;
+      }
+    }
+    System.out.println(max + 1);
+  }
 }
