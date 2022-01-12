c7c1628db02442befc03d607eb5904cf676f66ef
Jaehong Choe <qnalsfj@gmail.com>
2022-01-12 18:12:40 +0900

    [leetcode] Reverse Integer
    String을 사용하지 않고 풀어보았음.

--- src/leetcode/reverse_integer/Solution.java
+++ src/leetcode/reverse_integer/Solution.java
@@ -5,42 +5,69 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Solution {
+class Solution {
+  public int reverse(int x) {
 
-  public static void main(String[] args) {
-    System.out.println(reverse(-123));
-  }
-  public static int reverse(int x) {
-    String plusMinusSign = "";
-    String str = Integer.toString(x);
-    String[] strArr;
-
-    if (str.contains("-")) {
-      plusMinusSign = str.substring(0, 1);
-      str = str.substring(1);
-    }
-    if (str.lastIndexOf("0") == str.length() - 1) {
-      str = str.substring(0, str.length() - 1);
+    List<Integer> numbers = new ArrayList<>();
+    while (x != 0) {
+      numbers.add(x % 10);
+      x /= 10;
     }
 
-    List<String> strs = Arrays.asList(str.split(""));
-    Collections.reverse(strs);
+    long reversedNumber = 0L;
+    int exponent = numbers.size() - 1;
 
-    StringBuilder sb = new StringBuilder();
-    for (String s : strs) {
-      sb.append(s);
+    for (Integer num : numbers) {
+      if (num == 0) {
+        exponent--;
+        continue;
+      } else {
+        reversedNumber += Long.valueOf(num) * (long) Math.pow(10, exponent);
+      }
+      exponent--;
+      System.out.println(reversedNumber);
+
     }
 
-    String returnStr = plusMinusSign + sb.toString();
-    if (returnStr.equals("")) {
-      return 0;
-    }
-    if (Long.parseLong(returnStr) < (int) Math.pow(-2, 31) ||
-        (int) Math.pow(2, 32) - 1 < Long.parseLong(returnStr)) {
+    if (reversedNumber == 0 || reversedNumber > Integer.MAX_VALUE
+        || reversedNumber < Integer.MIN_VALUE) {
       return 0;
     }
 
-    return Integer.parseInt(returnStr);
-  }
 
+    return (int)reversedNumber;
+
+
+
+//         String plusMinusSign = "";
+//         String str = Integer.toString(x);
+//         String[] strArr;
+
+//         if (str.contains("-")) {
+//           plusMinusSign = str.substring(0, 1);
+//           str = str.substring(1);
+//         }
+//         if (str.lastIndexOf("0") == str.length() - 1) {
+//           str = str.substring(0, str.length() - 1);
+//         }
+
+//         List<String> strs = Arrays.asList(str.split(""));
+//         Collections.reverse(strs);
+
+//         StringBuilder sb = new StringBuilder();
+//         for (String s : strs) {
+//           sb.append(s);
+//         }
+
+//         String returnStr = plusMinusSign + sb.toString();
+//         if (returnStr.equals("")) {
+//           return 0;
+//         }
+//         if (Long.parseLong(returnStr) < (int) Math.pow(-2, 31) ||
+//             (int) Math.pow(2, 32) - 1 < Long.parseLong(returnStr)) {
+//           return 0;
+//         }
+
+//         return Integer.parseInt(returnStr);
+  }
 }
