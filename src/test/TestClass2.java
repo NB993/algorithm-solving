aa68f2d42a0f329bd7301bbe84f29351d10ba3d5
Jaehong Choe <qnalsfj@gmail.com>
2022-01-16 19:00:56 +0900

    [test] (#5) resolve #5
    왜 안닫혔지? 이슈 번호를 종료키워드 뒤에 적어야 하나?

--- src/test/TestClass2.java
+++ src/test/TestClass2.java
@@ -5,6 +5,8 @@ public class TestClass2 {
     public static void main(String[] args) {
         System.out.println("이슈 발행 전에는 들어가지 않나봄");
         System.out.println("aa");
+        System.out.println("aa");
+
     }
 
 }
